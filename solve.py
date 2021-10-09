eab2c399e7c11ff20403f1f90790e47d5d14649d
mlefebvre <michael.lefebvre.31@gmail.com>
2021-10-08 22:27:50 -0400

    install data files too

--- solve.py
+++ solve.py
@@ -23,13 +23,5 @@ def main():
     return 0
 
 
-"""
-for i in range(1, 10):
-    if i < 10:
-        problem_str = f"problem0{i}.problem0{i}()"
-    else:
-        problem_str = f"problem{i}.problem{i}()"
-    eval(problem_str)
-"""
 if __name__ == "__main__":
     sys.exit(main())
